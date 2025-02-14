24e433c939c2a5364c389e81092dba8b052fb2b5
CompScience <tim.neumueller1901@gmail.com>
2025-02-14 13:12:14 +0100

    Angfular Ädnerungen

---
+++ frontend/karma.conf.js
@@ -0,0 +1,14 @@
+module.exports = function(config) {
+    config.set({
+      // ... deine bisherigen Konfigurationen ...
+      browsers: ['ChromeHeadlessNoSandbox'],
+      customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox', '--disable-gpu']
+        }
+      },
+      // ...
+    });
+  };
+  
