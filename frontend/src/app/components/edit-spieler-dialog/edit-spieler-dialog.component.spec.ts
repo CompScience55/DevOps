9daf99109a509b9a44b462ecbe940bd25e85dcfa
CompScience <tim.neumueller1901@gmail.com>
2025-03-13 11:35:10 +0100

    Tests für Spieler und Edit Component

--- frontend/src/app/components/edit-spieler-dialog/edit-spieler-dialog.component.spec.ts
+++ frontend/src/app/components/edit-spieler-dialog/edit-spieler-dialog.component.spec.ts
@@ -9,13 +9,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 describe('EditSpielerDialogComponent', () => {
   let component: EditSpielerDialogComponent;
   let fixture: ComponentFixture<EditSpielerDialogComponent>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<EditSpielerDialogComponent>>;
 
   beforeEach(async () => {
+    dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
     await TestBed.configureTestingModule({
       imports: [EditSpielerDialogComponent, MatDialogModule, CommonModule],
       providers: [
-        { provide: MAT_DIALOG_DATA, useValue: {} },
-        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, 
+          useValue: { name: 'Max Mustermann', geburtsjahr: 1990, stadt: 'Musterstadt', land: 'Musterland' } },
+        { provide: MatDialogRef, useValue: dialogRefSpy },
         provideHttpClientTesting() ,
         provideAnimationsAsync()
       ],
@@ -29,5 +32,51 @@ describe('EditSpielerDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form with provided data', () => {
+    expect(component.editSpielerForm.value).toEqual({
+      name: 'Max Mustermann',
+      geburtsjahr: 1990,
+      stadt: 'Musterstadt',
+      land: 'Musterland'
+    });
+  });
+
+  it('should close the dialog with updated data when onSave is called and form is valid', () => {
+    component.editSpielerForm.setValue({
+      name: 'Erika Musterfrau',
+      geburtsjahr: 1985,
+      stadt: 'Beispielstadt',
+      land: 'Beispielland'
+    });
+
+    component.onSave();
+
+    expect(dialogRefSpy.close).toHaveBeenCalledWith({
+      name: 'Erika Musterfrau',
+      geburtsjahr: 1985,
+      stadt: 'Beispielstadt',
+      land: 'Beispielland'
+    });
+  });
+
+  it('should not close the dialog when onSave is called and form is invalid', () => {
+    component.editSpielerForm.setValue({
+      name: '',
+      geburtsjahr: 1985,
+      stadt: 'Beispielstadt',
+      land: 'Beispielland'
+    });
+
+    component.onSave();
+
+    expect(dialogRefSpy.close).not.toHaveBeenCalled();
+  });
+
+  it('should close the dialog without data when onCancel is called', () => {
+    component.onCancel();
+
+    expect(dialogRefSpy.close).toHaveBeenCalledWith();
   });
 });
--- frontend/src/app/components/spieler/spieler.component.spec.ts
+++ frontend/src/app/components/spieler/spieler.component.spec.ts
@@ -1,29 +1,110 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SpielerComponent } from './spieler.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { Spieler, SpielerService } from '../../service/spieler.service';
+import { MatDialog } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SpielerCardComponent } from '../spieler-card/spieler-card.component';
+import { SpielerDialogComponent } from '../spieler-dialog/spieler-dialog.component';
+import { of } from 'rxjs';
+import { EditSpielerDialogComponent } from '../edit-spieler-dialog/edit-spieler-dialog.component';
 
 describe('SpielerComponent', () => {
   let component: SpielerComponent;
   let fixture: ComponentFixture<SpielerComponent>;
+  let spielerService: jasmine.SpyObj<SpielerService>;
+  let dialog: jasmine.SpyObj<MatDialog>;
 
   beforeEach(async () => {
+    const spielerServiceSpy = jasmine.createSpyObj('SpielerService', ['getSpieler', 'updateSpieler', 'deleteSpieler', 'createSpieler']);
+    const dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+
     await TestBed.configureTestingModule({
-      imports: [SpielerComponent],
+      imports: [
+        SpielerComponent,
+        NoopAnimationsModule,
+        SpielerCardComponent
+      ],
       providers: [   
         provideHttpClient(),
-        provideHttpClientTesting() 
+        provideHttpClientTesting(),
+        { provide: SpielerService, useValue: spielerServiceSpy },
+        { provide: MatDialog, useValue: dialogSpy }
       ],
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SpielerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    spielerService = TestBed.inject(SpielerService) as jasmine.SpyObj<SpielerService>;
+    dialog = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load players on initialization', () => {
+    const mockSpieler: Spieler[] = [
+      { id: 1, name: 'Max Mustermann', geburtsjahr: 1990, stadt: 'Musterstadt', land: 'Musterland' }
+    ];
+    spielerService.getSpieler.and.returnValue(of(mockSpieler));
+
+    fixture.detectChanges();
+
+    expect(component.spielerListe).toEqual(mockSpieler);
+    expect(spielerService.getSpieler).toHaveBeenCalled();
+  });
+
+  it('should open edit dialog and update player on save', () => {
+    const mockSpieler: Spieler = { id: 1, name: 'Max Mustermann', geburtsjahr: 1990, stadt: 'Musterstadt', land: 'Musterland' };
+    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
+    dialogRefSpy.afterClosed.and.returnValue(of(mockSpieler));
+    dialog.open.and.returnValue(dialogRefSpy);
+  
+    // updateSpieler gibt ein Observable zurück:
+    spielerService.updateSpieler.and.returnValue(of(mockSpieler));
+    // Wichtig: getSpieler muss ebenfalls ein Observable zurückgeben, da loadSpieler() später aufgerufen wird:
+    spielerService.getSpieler.and.returnValue(of([]));
+  
+    component.onEdit(mockSpieler);
+  
+    expect(dialog.open).toHaveBeenCalledWith(EditSpielerDialogComponent, {
+      width: '400px',
+      maxHeight: '80vh',
+      data: { id: 1, name: 'Max Mustermann', geburtsjahr: 1990, stadt: 'Musterstadt', land: 'Musterland' },
+      panelClass: 'no-scroll-dialog'
+    });
+    expect(dialogRefSpy.afterClosed).toHaveBeenCalled();
+    expect(spielerService.updateSpieler).toHaveBeenCalledWith(mockSpieler.id!, mockSpieler);
+  });
+
+  it('should delete player', () => {
+    const mockSpieler: Spieler = { id: 1, name: 'Max Mustermann', geburtsjahr: 1990, stadt: 'Musterstadt', land: 'Musterland' };
+    component.spielerListe = [mockSpieler];
+    spielerService.deleteSpieler.and.returnValue(of(void 0));
+
+    component.onDelete(mockSpieler);
+
+    expect(spielerService.deleteSpieler).toHaveBeenCalledWith(mockSpieler.id!);
+    expect(component.spielerListe).not.toContain(mockSpieler);
+  });
+
+  it('should open new player dialog and add player on save', () => {
+    const mockSpieler: Spieler = { id: 2, name: 'Erika Musterfrau', geburtsjahr: 1985, stadt: 'Beispielstadt', land: 'Beispielland' };
+    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
+    dialogRefSpy.afterClosed.and.returnValue(of(mockSpieler));
+    dialog.open.and.returnValue(dialogRefSpy);
+    spielerService.createSpieler.and.returnValue(of(mockSpieler));
+
+    component.onNewSpieler();
+
+    expect(dialog.open).toHaveBeenCalledWith(SpielerDialogComponent, {
+      width: '400px',
+      data: {}
+    });
+    expect(dialogRefSpy.afterClosed).toHaveBeenCalled();
+    expect(spielerService.createSpieler).toHaveBeenCalledWith(mockSpieler);
+    expect(component.spielerListe).toContain(mockSpieler);
   });
 });
